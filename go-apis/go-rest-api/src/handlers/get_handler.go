dac80d39d8129cf50e13ea6c88bdd4b2b823165c
ns185365_ncrvoyix <ns185365@ncrvoyix.com>
2025-09-27 15:44:37 +0530

    fix auto trigger action

--- go-apis/go-rest-api/src/handlers/get_handler.go
+++ go-apis/go-rest-api/src/handlers/get_handler.go
@@ -7,6 +7,6 @@ import (
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	response := map[string]string{"message": "Hello, World! version to latest"}
+	response := map[string]string{"message": "Hello, World! version to"}
 	json.NewEncoder(w).Encode(response)
 }
